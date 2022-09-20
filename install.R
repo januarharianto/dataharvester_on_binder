72acae4156ae623d30de9dbd6e4a76752b778778
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-20 22:00:13 +1000

    build: swap to feature/binder branch

--- install.R
+++ install.R
@@ -1,32 +1,8 @@
 install.packages("remotes")
 install.packages("startup")
-remotes::install_github("sydney-informatics-hub/dataharvester", ref = "develop")
+remotes::install_github("sydney-informatics-hub/dataharvester",
+  ref = "feature/binder"
+)
 reticulate::install_miniconda()
 reticulate::conda_remove("r-reticulate")
 reticulate::conda_create(envname = "r-reticulate", python_version = "3.9")
-reticulate::conda_install(
-  envname = "r-reticulate",
-  packages = c(
-    "ipykernel",
-    "earthengine-api",
-    "google-cloud-sdk",
-    "geopandas",
-    "netcdf4",
-    "numba",
-    "owslib",
-    "rioxarray==1.2.10"
-  )
-)
-reticulate::conda_install(
-  envname = "dataharvester",
-  packages = c(
-    "eemont",
-    "geemap",
-    "geedim",
-    "wxee",
-    "alive-progress",
-    "termcolor"
-  ),
-  forge = FALSE,
-  pip = TRUE
-)
