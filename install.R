38ed5371cdb20e28f04fcb140cc54f06d71e49ea
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-20 22:43:20 +1000

    build: removed `startup` package dependency

--- install.R
+++ install.R
@@ -1,8 +1,6 @@
 install.packages("remotes")
-install.packages("startup")
 remotes::install_github("sydney-informatics-hub/dataharvester",
   ref = "feature/binder"
 )
 reticulate::install_miniconda()
-reticulate::conda_remove("r-reticulate")
-reticulate::conda_create(envname = "r-reticulate", python_version = "3.9")
+reticulate::conda_install(envname = "r-reticulate", packages = "python==3.9")
