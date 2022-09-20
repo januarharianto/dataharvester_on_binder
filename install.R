24a269a50ac7ec38e20075eb16c024c099154ad3
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-20 13:16:07 +1000

    build: try to install miniconda and r-reticulate env

--- install.R
+++ install.R
@@ -1,2 +1,4 @@
 install.packages("remotes")
 remotes::install_github("sydney-informatics-hub/dataharvester", ref = "develop")
+reticulate::install_miniconda()
+dataharvester::initialise_harvester(earthengine = FALSE)
