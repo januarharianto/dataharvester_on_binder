69d17e3b922d4f042a379e95339090087c1e1871
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-20 13:55:45 +1000

    fix: `r-reticulate` env could not install via environment.yml

--- install.R
+++ install.R
@@ -2,6 +2,31 @@ install.packages("remotes")
 install.packages("startup")
 remotes::install_github("sydney-informatics-hub/dataharvester", ref = "develop")
 reticulate::install_miniconda()
-startup::restart() 
 reticulate::conda_remove("r-reticulate")
-reticulate::conda_create(envname = "r-reticulate", environment = "environment.yml")
+reticulate::conda_create(envname = "r-reticulate", python_version = "3.9")
+reticulate::conda_install(
+  envname = "r-reticulate",
+  packages = c(
+    "ipykernel",
+    "earthengine-api",
+    "google-cloud-sdk",
+    "geopandas",
+    "netcdf4",
+    "numba",
+    "owslib",
+    "rioxarray"
+  )
+)
+reticulate::conda_install(
+  envname = "dataharvester",
+  packages = c(
+    "eemont",
+    "geemap",
+    "geedim",
+    "wxee",
+    "alive-progress",
+    "termcolor"
+  ),
+  forge = FALSE,
+  pip = TRUE
+)
