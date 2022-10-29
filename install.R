afe7c0dbc24c9f078bc970ca84bcc72b22a4ad81
Januar Harianto <januar.harianto@sydney.edu.au>
2022-10-30 09:55:27 +1000

    build: testing if `initialise_harvester()` now works

--- install.R
+++ install.R
@@ -1,6 +1,5 @@
+install.packages("reticulate")
 install.packages("remotes")
-remotes::install_github("sydney-informatics-hub/dataharvester",
-  ref = "feature/binder"
-)
-reticulate::install_miniconda()
-reticulate::conda_install(envname = "r-reticulate", packages = "python==3.9")
+remotes::install_github("sydney-informatics-hub/dataharvester")
+reticulate::install_miniconda(update = FALSE, force = TRUE)
+dataharvester::initialise_harvester(envname = "r-reticulate")
